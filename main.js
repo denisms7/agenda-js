58a7366f5f0c80f80e2b9181eb8bb165178fd7ce
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2022-10-15 20:53:26 -0300

    Changed nao aceitacao de duplicado

--- main.js
+++ main.js
@@ -2,15 +2,25 @@ const fmr = document.getElementById('formulario')
 const nome = document.getElementById('nome')
 const telefone = document.getElementById('telefone')
 const corpoTabela = document.querySelector('tbody')
+const avisos = document.getElementById('aviso')
 let linhas = ''
+let lista = []
 
 
 
 fmr.addEventListener('submit', function(e){
     e.preventDefault()
-    linha(nome.value, telefone.value)
-})
+    
 
+    if (lista.includes(nome.value)) {
+        aviso(true)
+    } else {
+        linha(nome.value, telefone.value)
+        aviso(false)
+    }
+
+
+})
 
 function linha(aa, bb) {
     let linha = '<tr>'
@@ -19,5 +29,19 @@ function linha(aa, bb) {
     linha += '<tr>'
     linhas += linha 
     corpoTabela.innerHTML = linhas
+    lista.push(aa)
 }
+
+function aviso(aa) {
+    if (aa) {
+        avisos.style.display = 'block'
+        avisos.innerHTML = `O nome ${nome.value} ja esta cadastrado`
+        nome.focus()
+    } else {
+        avisos.style.display = 'none'
+    }
+}
+
+
+
 
