ccde720569dc2e412d68e7ed40dbdb93b097d754
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2022-10-15 20:32:20 -0300

    Added funcao adicionar linhas

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-const fmr = document.getElementsByTagName('form')
+const fmr = document.getElementById('formulario')
 const nome = document.getElementById('nome')
 const telefone = document.getElementById('telefone')
+const corpoTabela = document.querySelector('tbody')
+let linhas = ''
+
+
+
+fmr.addEventListener('submit', function(e){
+    e.preventDefault()
+    linha(nome.value, telefone.value)
+})
+
+
+function linha(aa, bb) {
+    let linha = '<tr>'
+    linha +=  `<td>${aa}</td>`
+    linha +=  `<td>${bb}</td>`
+    linha += '<tr>'
+    linhas += linha 
+    corpoTabela.innerHTML = linhas
+}
 
